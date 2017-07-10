13e1433290f3bc2e1063a826b1cc4f69db1909b2
terehova <marinaterechova1974@gmail.com>
2017-07-10 17:52:22 +0000

    main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+def maximum(lst):
+    return max(lst)
+
+def minimum(lst):
+   return min(lst)
+
+def count(lst):
+  return len(lst)
+
+def check_age(age)
+  if age >= 18:
+    return False
+  else:
+    return True
